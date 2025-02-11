3a3f3aa81efeec1b1933734afbf5a95c2abbbc06
chanbin <rcq0111@gmail.com>
2025-02-11 16:07:36 +0900

    v1.0 day02

--- day02.py
+++ day02.py
@@ -1,26 +1,27 @@
-def is_prime(num):
-    """
-    소수 여부를 판정해서 소수면 True 소수가아니면 False를 리턴하는 함수.
-    :param num: integer number
-    :return:  boolean type
-    """
-    if num >= 2:
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-                # count = count + 1
-                #is_prime = False
-                #break
-            #print(i, end=' ')
-    else:
-        return False
-    return True
 
-# main
-help(is_prime)
-3n = int(input("input numbers : "))
+# univ = "Inha University"
+# print(univ)
+# print(univ[5])
+# #univ[5] = 'U' # immutable 바꿀 수 없음 / 문자열
+# subjects = ['python', 'c++', 'Liunx', 'data structure', 'database']
+# subjects[3] = 'data structure & algorithm' # mutable
+# print(subjects)
 
-if is_prime(n):
-    print(f"{n} is prime number")
-else:
-    print(f"{n} is not prime number")
+# print(0.1)
+# print(1e-1) # 10의 -1승
+# print(0.01)
+# print(1e-2)
+# print(314.1592)
+# print(0.3141592e3) # 10의 3승
+# print(21000)
+# print(21_000)
+
+#print(isinstance(55, int))
+
+# artists = ['BTS', '뉴진스', '핑클', 'SES', 'HOT', '블랙핑크']
+# groups = artists
+# print(artists, groups)
+# artists[2] = '세븐틴'
+# print(artists, groups)
+#shall copy 공유 참조 -> side effect
+
