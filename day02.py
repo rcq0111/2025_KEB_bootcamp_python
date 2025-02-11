919116446d1d93a051a83457105e1c3762e2e559
chanbin <rcq0111@gmail.com>
2025-02-11 16:24:07 +0900

    v1.1 day02

--- day02.py
+++ day02.py
@@ -1,27 +1,27 @@
+#for -> while
+#while 문으로 구간 소수를 출력하는 프로그램
 
-# univ = "Inha University"
-# print(univ)
-# print(univ[5])
-# #univ[5] = 'U' # immutable 바꿀 수 없음 / 문자열
-# subjects = ['python', 'c++', 'Liunx', 'data structure', 'database']
-# subjects[3] = 'data structure & algorithm' # mutable
-# print(subjects)
+number = int(input("Input number : "))
+is_prime = True  # int -> bool
+i = 2
+while i < number:
+    if number % i == 0:
+        is_prime = False  # remove +
+        break
+    #print(i, end=' ')
+    i = i + 1
 
-# print(0.1)
-# print(1e-1) # 10의 -1승
-# print(0.01)
-# print(1e-2)
-# print(314.1592)
-# print(0.3141592e3) # 10의 3승
-# print(21000)
-# print(21_000)
+if is_prime:  # remove ==
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is NOT prime number!')
+#main
+# numbers = input("숫자를 두개 입력하시오. : ").split(',')
+# n1 = int(numbers[0])
+# n2 = int(numbers[1])
+#
+# if n1 > n2:
+#     n1, n2 = n2, n1
 
-#print(isinstance(55, int))
 
-# artists = ['BTS', '뉴진스', '핑클', 'SES', 'HOT', '블랙핑크']
-# groups = artists
-# print(artists, groups)
-# artists[2] = '세븐틴'
-# print(artists, groups)
-#shall copy 공유 참조 -> side effect
 
