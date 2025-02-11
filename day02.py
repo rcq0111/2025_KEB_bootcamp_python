677a0d01fb2a8c7fcd3a1b6cd11ded7c80d1c45d
chanbin <rcq0111@gmail.com>
2025-02-11 10:34:30 +0900

    v0.4 day02 gugudan

---
+++ day02.py
@@ -0,0 +1,8 @@
+#dan = input("input dan : ")
+dan = int(input("input dan : "))
+for i in range(1, 10, 1):
+    print(f"{dan} * {i} = {dan*i}")
+
+#for dan in range(2, 10, 1):
+#    for i in range(1, 10, 1):
+#        print(f"{dan} * {i} = {dan*i}")
