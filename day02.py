65f5f22034ace87187f05ab426aa92797b26a633
chanbin <rcq0111@gmail.com>
2025-02-11 17:11:18 +0900

    v1.2 day02

--- day02.py
+++ day02.py
@@ -1,27 +1,26 @@
-#for -> while
-#while 문으로 구간 소수를 출력하는 프로그램
 
-number = int(input("Input number : "))
-is_prime = True  # int -> bool
-i = 2
-while i < number:
-    if number % i == 0:
-        is_prime = False  # remove +
-        break
-    #print(i, end=' ')
-    i = i + 1
+# prime number
+numbers = input("Input first second number : ").split(',')
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1
+while n1 < n2:
+    is_prime = True
 
-if is_prime:  # remove ==
-    print(f'{number} is prime number')
-else:
-    print(f'{number} is NOT prime number!')
-#main
-# numbers = input("숫자를 두개 입력하시오. : ").split(',')
-# n1 = int(numbers[0])
-# n2 = int(numbers[1])
-#
-# if n1 > n2:
-#     n1, n2 = n2, n1
+    if n1 < 2:
+        pass
+    else:
+        i = 2
+        while i < n1:
+            if n1 % i == 0:
+                is_prime = False
+                break
+            i = i +1
+        if is_prime: print(n1, end=' ')
+    n1 = n1 + 1
+
+
 
 
 
