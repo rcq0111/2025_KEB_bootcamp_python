e33634c0c66d90c2a8a63e0503440b4de6d383af
chanbin <rcq0111@gmail.com>
2025-02-11 10:56:31 +0900

    v0.7 day02 prime number

--- day02.py
+++ day02.py
@@ -1,10 +1,18 @@
 n = int(input("input numbers : "))
-count = 0
-for i in range(1, 1+n):
-    if n % i == 0:
-        count = count + 1
 
-if count == 2:
+is_prime = True
+
+if n>= 2:
+    for i in range(2, n):
+        if n % i == 0:
+            #count = count + 1
+            is_prime =False
+            break
+else:
+    is_prime = False
+
+#if count == 0:
+if is_prime:
     print(f"{n} is prime number")
 else:
     print(f"{n} is not prime number")
