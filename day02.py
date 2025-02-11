075f14a7848dd97a42d7f60d9cb07641dfd10156
chanbin <rcq0111@gmail.com>
2025-02-11 11:40:23 +0900

    v0.8 day02 prime number

--- day02.py
+++ day02.py
@@ -1,13 +1,18 @@
+import math
+#print(type(2**10)) #1024
+#print(type(16**0.5)) #4.0
+
 n = int(input("input numbers : "))
 
 is_prime = True
 
 if n>= 2:
-    for i in range(2, n):
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             #count = count + 1
             is_prime =False
             break
+        print(i, end=' ')
 else:
     is_prime = False
 
