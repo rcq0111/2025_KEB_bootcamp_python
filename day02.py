3779b78ce5b8743f1a205ee4a0541f55466fd226
chanbin <rcq0111@gmail.com>
2025-02-11 17:24:33 +0900

    v1.4 day02

--- day02.py
+++ day02.py
@@ -1,3 +1,14 @@
+def my_pow(b, e):
+    """
+    밑과 지수를  입력 받아 거듭제곱을 한 결과를 실수 형태로 리턴 받는다.
+    :param b:
+    :param e:
+    :return:
+    """
+    return b**e
+
+
+
 
 # prime number
 
@@ -13,7 +24,7 @@ while n1 <= n2:
         pass
     else:
         i = 2
-        while i < int(pow(n1,0.5)+1):
+        while i < int(my_pow(n1,0.5)+1):
             if n1 % i == 0:
                 is_prime = False
                 break
