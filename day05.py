4b4026b554fa88b339263861a6631962f7e3ced5
chanbin <rcq0111@gmail.com>
2025-02-14 17:20:19 +0900

    v2.6 tgmath,

--- day05.py
+++ day05.py
@@ -1,1 +1,5 @@
 import tgmath
+
+if __name__ == "__main__":
+    print(tgmath.my_abs(-99))
+    print(tgmath.fibonacci_loop(10))
--- tgmath.py
+++ tgmath.py
@@ -8,4 +8,32 @@ def my_abs(n) -> int:
         return -n
     return n
 
-print(my_abs(-99), my_abs(-909), my_abs(0))
+#print(my_abs(-99), my_abs(-909), my_abs(0))
+
+def fibonacci_recursion(n) -> int:
+    """
+    피보나치 수 계산함수 (재귀함수 버전)
+    :param n:
+    :return: 피보나치 계산 결과 값
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+
+
+def fibonacci_loop(n) -> int:
+    """
+    피보나치 수 계산함수 (반복문 버전)
+    :param n:
+    :return: 피보나치 계산 결과 값
+    """
+    n_list=[0 ,1]
+    for i in range(n+1):
+        n_list.append(n_list[i] + n_list[i + 1])
+    return n_list[n]
+
+if __name__ != "__main__": # main 현재 실행시키는 파일
+    print("tgmath.py 파일을 실행하셨습니다.")
