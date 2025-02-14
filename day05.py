72c87e6f2bac0d5872b2c60ebd12ff2b1417c947
chanbin <rcq0111@gmail.com>
2025-02-14 17:22:36 +0900

    v2.7 tgmath,

--- day05.py
+++ day05.py
@@ -1,5 +1,8 @@
-import tgmath
+#import tgmath
+import tgmath as tg
 
 if __name__ == "__main__":
-    print(tgmath.my_abs(-99))
-    print(tgmath.fibonacci_loop(10))
+    # print(tgmath.my_abs(-99))
+    # print(tgmath.fibonacci_loop(10))
+    print(tg.my_abs(-99))
+    print(tg.fibonacci_loop(10))
