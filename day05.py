5d59eab52268474005fbd789efaac3a3c28e5a86
chanbin <rcq0111@gmail.com>
2025-02-14 17:09:37 +0900

    v2.5 tgmath

--- day05.py
+++ day05.py
@@ -1,5 +1,1 @@
-# 상속 heritage
-
-# 연관 관계
-# 강한 연결 관계 : composition (ex)자동차 팔때 다같이 팜/ 전체를 묶음)
-# 약한 연결 괸계 : aggregation (컴퓨터와 키보드)
+import tgmath
---
+++ tgmath.py
@@ -0,0 +1,11 @@
+def my_abs(n) -> int:
+    """
+    Return Absolute value of parameter n
+    :param n:
+    :return: absolute value
+    """
+    if n < 0:
+        return -n
+    return n
+
+print(my_abs(-99), my_abs(-909), my_abs(0))
