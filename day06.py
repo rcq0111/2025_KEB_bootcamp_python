ea5d0ee8e95533342454b7586b80ec85c6fc7d33
chanbin <rcq0111@gmail.com>
2025-02-17 14:05:35 +0900

    v3.5 day06

--- day06.py
+++ day06.py
@@ -1,9 +1,35 @@
-# O(1)
-def sum(n):
-    return n * (n + 1) // 2
+def print_poly(f_x, t_x) -> str:
+    #term = len(f_x) - 1
+    poly_expression = "f(x) = "
 
-print("숫자를 입력하시오 : ", end='')
-n = int(input())
-print(sum(n))
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        term = t_x[i]
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        #term = term - 1
 
-# f(n) = x^3 + 1_000_000_000 O표기법이 항상 적용 되는 것은 아니다.
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x, t_x) -> int:
+    return_value = 0
+    #term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        term = t_x[i]
+        return_value += coefficient * pow(x_value, term)
+        #term = term - 1
+
+    return return_value
+
+
+#fx = [2, 3, 4, 0, -9]
+fx = [2, 5, -9, 11]
+tx = [20, 7 , 2, 0]
+
+if __name__ == "__main__":
+    print(print_poly(fx,tx))
+    print(calculation_poly(int(input("x 값 : ")), fx, tx))
