4c4d80b16b86611e532525453dab9f6c518c2c36
chanbin <rcq0111@gmail.com>
2025-02-17 09:50:37 +0900

    v2.9 day06

--- day06.py
+++ day06.py
@@ -4,16 +4,17 @@ def is_even(n) -> bool :
     :param n: 판정할 정수
     :return: 짝수면 True, 홀수면 False
     """
-    if n % 2 == 0 :
-        return True
-    return  False
+    return not n & 1
+    # if n % 2 == 0 :
+    #     return True
+    # return  False
 
-# n = int(input())
-# print(is_even(n))
+n = int(input())
+print(is_even(n))
 
-a = 10 # 0000 1010
-b = 11 # 0000 1011 & -> 0000 1010
-# bit operation
-print(a & b)
-print(a | b) # | -> 0000 1011
-print(a ^ b) # ^ -> 0000 0001 exclusiveOr
+# a = 10 # 0000 1010
+# b = 11 # 0000 1011 & -> 0000 1010
+# # bit operation
+# print(a & b)
+# print(a | b) # | -> 0000 1011
+# print(a ^ b) # ^ -> 0000 0001 exclusiveOr
