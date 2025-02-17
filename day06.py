391adf217db2e1085b79deae4e24ef6217581671
chanbin <rcq0111@gmail.com>
2025-02-17 14:55:52 +0900

    v3.7 day06 ëfibonacci_memo_dp

--- day06.py
+++ day06.py
@@ -26,6 +26,53 @@ def calculation_poly(x_value, f_x, t_x) -> int:
 fx = [-2, 5, -9, 11]
 tx = [20, 7 , 2, 0]
 
-if __name__ == "__main__":
-    print(print_poly(fx,tx))
-    print(calculation_poly(int(input("x 값 : ")), fx, tx))
+# if __name__ == "__main__":
+#     print(print_poly(fx,tx))
+#     print(calculation_poly(int(input("x 값 : ")), fx, tx))
+
+memo = dict()
+
+def fibonacci_recursion(n) -> int:
+    """
+    피보나치 수 계산함수 (재귀함수 버전)
+    :param n:
+    :return: 피보나치 계산 결과 값
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursion(n - 2) + fibonacci_recursion(n - 1)
+
+def fibonacci_recursion_dp(n) -> int:
+    """
+    딕셔너리에 이미 계산된 값이 있으면 그 값을 리턴
+    :param n:
+    :return: 0, 1이 오면 그 값을 바로 리턴
+    """
+    if n in memo:
+        return memo[n]
+    elif n <= 1: # 0이나 1이오면 바로 그값을 리턴
+        return n
+    else:
+        memo[n] = fibonacci_recursion_dp(n - 2) + fibonacci_recursion_dp(n - 1) # 딕셔너리에 계산된 값이 없을 경우 딕셔너리에 추가
+        return memo[n]
+
+def fibonacci_loop(n) -> int:
+    """
+    피보나치 수 계산함수 (반복문 버전)
+    :param n:
+    :return: 피보나치 계산 결과 값
+    """
+    n_list = [0, 1]
+    for i in range(n + 1):
+        n_list.append(n_list[i] + n_list[i + 1])
+
+    return n_list[n]
+
+
+n = int(input())
+print(fibonacci_loop(n))
+print(fibonacci_recursion(n))
+print(fibonacci_recursion_dp(n))
