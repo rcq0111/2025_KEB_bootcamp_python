e84182f706e27a2645133a7e1003a3045f9348d7
chanbin <rcq0111@gmail.com>
2025-02-17 16:53:18 +0900

    v3.9 day6 openFile

--- day06.py
+++ day06.py
@@ -1,2 +1,30 @@
-array = [9 , -11, 8, 7]
-print(id(array[0]), id(array[1]), id(array[2]), id(array[3]))
+# try:
+#     file = input("file name : ")
+#     with open(file, 'r') as fp:
+#         #fp = open(file, 'r')
+#         readme_list = fp.readline().split(' ')
+#         for i in readme_list:
+#             print(i)
+#         print(readme_list)
+#         #fp.close()
+# except FileNotFoundError as err:
+#     print(err)
+#     #print(f"{file} is not exist. {err}")
+import random
+
+try:
+    with open("students.csv", 'r') as fp:
+        students_list = fp.readlines()
+        students_list.remove("이상혁\n")
+        students_list.remove("조윤하\n")
+        students_list.remove("김철중\n")
+        students_list.remove("김현민\n")
+        students_list.remove("김찬빈\n")
+
+        for _ in range(3):
+            random_pick = random.choice(students_list)
+            print(random_pick, end='')
+            students_list.remove(random_pick)
+
+except FileNotFoundError as err:
+    print(err)
