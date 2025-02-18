9d1c251c0db8dad6479c625da44faa4357526e0c
chanbin <rcq0111@gmail.com>
2025-02-18 13:09:26 +0900

    v4.4 day7 linkedlist

--- day07.py
+++ day07.py
@@ -1,22 +1,16 @@
-def a(i):
-    j = 9
-    print(i, j)
+import random
+
+
 class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
-def b(k):
-    a(1)
-    print(k)
-def main():
-    print("start")
-    b(3)
-    print("end")
     def append(self, data):
         if not self.head:
             self.head = Node(data)
@@ -26,10 +20,32 @@ def main():
             current = current.next  # move
         current.next = Node(data)
 
+    def search(self, target) -> bool:
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
+
+
+    def __str__(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node = node.next
+        return "end"
+
+
 
 if __name__ == "__main__":
-    main()
     l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
+    i = 0
+    while i < 20:
+        n = random.randint(1, 20)
+        l.append(n)
+        print(n, end=' ')
+        i = i + 1
+    #print(l)
+    print(l.search(10))
