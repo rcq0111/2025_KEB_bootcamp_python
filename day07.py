3a93223909cc06a972721f7524af58baa7004223
chanbin <rcq0111@gmail.com>
2025-02-18 13:07:36 +0900

    v4.3 day7 linkedlist

--- day07.py
+++ day07.py
@@ -1,18 +1,35 @@
-class Stack:
+def a(i):
+    j = 9
+    print(i, j)
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
     def __init__(self):
-        self.items = list()
-    def push(self, data):
-        self.items.append(data)
-    def pop(self):
-        return self.items.pop()
-    def size(self):
-        return len(self.items)
-    def peek(self):
-        return self.items[-1]
-s1 = Stack()
-s1.push(-9)
-s1.push(11)
-s1.push(977)
-print(s1.pop())
-print(s1.peek())
-print(s1.peek())
+        self.head = None
+
+def b(k):
+    a(1)
+    print(k)
+def main():
+    print("start")
+    b(3)
+    print("end")
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:  # if next node exist
+            current = current.next  # move
+        current.next = Node(data)
+
+
+if __name__ == "__main__":
+    main()
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
