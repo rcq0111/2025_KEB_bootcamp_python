7c6b3774222bac3f15b5024357a7aadd39d10951
chanbin <rcq0111@gmail.com>
2025-02-21 13:38:56 +0900

    v6.5 day10 sort

--- day10.py
+++ day10.py
@@ -10,6 +10,21 @@ def time_decorator(func):
         return r
     return wrapper
 
+def quick_sort(l):
+    n = len(l)
+    if n <= 1:
+        return l
+
+    pivot = l[n // 2]
+    left, right = list(), list()
+
+    for i in l:
+        if i < pivot:
+            left.append(i)
+        elif i > pivot:
+            right.append(i)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
 
 @time_decorator
 def insertion_sort(l):
@@ -21,7 +36,6 @@ def insertion_sort(l):
             #print(i, end=' ')
         l[i] = value
     return l
-
 
 @time_decorator
 def bubble_sort(l):
@@ -38,11 +52,15 @@ def bubble_sort(l):
 
 
 
-lists1 = [random.randint(1, 100000) for _ in range(10000)]
+lists1 = [random.randint(1, 10000) for _ in range(1000)]
 lists2 = lists1.copy()
+lists3 = lists1.copy()
 bubble_sort(lists1)
 insertion_sort(lists2)
-
+s = time.time()
+quick_sort(lists3)
+e = time.time()
+print(f'실행시간 : {e - s}초')
 # print(bubble_sort([33, 8, -11, 9, 1]))
 # print(bubble_sort([13, 15, 20, 99, 100]))
 # print(insertion_sort([33, 8, -11, 9, 1]))
