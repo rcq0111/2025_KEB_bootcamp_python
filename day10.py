44481821b40516c4790404a025f278537ccf3146
chanbin <rcq0111@gmail.com>
2025-02-21 10:23:24 +0900

    v6.1 day10 sort

--- day10.py
+++ day10.py
@@ -1,8 +1,9 @@
 def bubble_sort(l):
     for i in range(len(l) - 1):
-        for j in range(len(l) - 1):
+        for j in range(len(l) - 1 - i):
             if l[j] > l[j + 1]:
                 l[j], l[j + 1] = l[j + 1], l[j]
+            print(j, end=' ')
     return l
 
-print(bubble_sort([8, -11, 9, 1]))
+print(bubble_sort([33, 8, -11, 9, 1]))
